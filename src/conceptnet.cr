2f2680c930ae3b56b24a62cac1c935a1924638a7
Rémy Marronnier <rmarronnier@gmail.com>
2019-06-26 17:47:21 +0200

    get node method

--- src/conceptnet.cr
+++ src/conceptnet.cr
@@ -3,22 +3,17 @@ require "json"
 
 module ConceptNet
   class Client
-    getter address : String!
+    property address = "http://api.conceptnet.io"
 
-    property language : String!
+    property language = "en"
 
-
-    def initialize(address = "https://api.conceptnet.io", language = "en")
-    @address = address
-    @language = language
+    def initialize(language = "en")
+      @language = language.not_nil!
     end
 
     def get_node(node)
-      response = HTTP::Client.get @address + "/c/" + @language + "/" + node.not_nil!
+      response = HTTP::Client.get @address + "/c/" + @language + "/" + node
       obj = JSON.parse(response.body)
     end
-  
   end
-
-
 end
