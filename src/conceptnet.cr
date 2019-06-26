c42d047557497ab7c37c37066095b0add2d9130b
Rémy Marronnier <rmarronnier@gmail.com>
2019-06-26 18:26:49 +0200

    Last Methods - WIP

--- src/conceptnet.cr
+++ src/conceptnet.cr
@@ -1,6 +1,8 @@
 require "http/client"
 require "json"
 
+# https://github.com/commonsense/conceptnet5/wiki/API
+
 module ConceptNet
   class Client
     property address = "http://api.conceptnet.io"
@@ -15,5 +17,17 @@ module ConceptNet
       response = HTTP::Client.get @address + "/c/" + @language + "/" + node
       obj = JSON.parse(response.body)
     end
+
+    # def query(startURI, endURI, rel, node, other, sources)
+    #   response = HTTP::Client.get @address + startURI + endURI + "/" + node
+    #   obj = JSON.parse(response.body)
+    # end
+
+    # def related
+    # end
+
+    # def relatedness
+    # end
+
   end
 end
